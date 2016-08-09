d428db070ac9a2f96cb62800c587f0b96fd2e2bc
florent.tollin <florent.tollin@fwa.eu>
2016-08-10 00:29:27 +0200

    Little changes

--- DarkEngines.Server/Program.cs
+++ DarkEngines.Server/Program.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 
 namespace DarkEngines.Server
 {
@@ -11,11 +12,16 @@ namespace DarkEngines.Server
     {
         public static void Main(string[] args)
         {
+			var configuration = new ConfigurationBuilder()
+				.AddCommandLine(args)
+				.Build();
+
             var host = new WebHostBuilder()
                 .UseKestrel()
                 .UseContentRoot(Directory.GetCurrentDirectory())
-                .UseIISIntegration()
-                .UseStartup<Startup>()
+				.UseConfiguration(configuration)
+				.UseIISIntegration()
+				.UseStartup<Startup>()
                 .Build();
 
             host.Run();
